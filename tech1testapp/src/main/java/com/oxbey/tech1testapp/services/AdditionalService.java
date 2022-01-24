f67e31ad0670eb9cb5fe2a289ea193cb932b28e5
Andrii Filipchuk <andrii.filipchuk@everymatrix.com>
2022-01-24 22:46:48 +0200

    add AdditionalService, change ArticleService

---
+++ tech1testapp/src/main/java/com/oxbey/tech1testapp/services/AdditionalService.java
@@ -0,0 +1,11 @@
+package com.oxbey.tech1testapp.services;
+
+public class AdditionalService {
+    private final String str;
+    private final int number;
+
+    AdditionalService(String str, int number) {
+        this.number = number;
+        this.str = str;
+    }
+}
--- tech1testapp/src/main/java/com/oxbey/tech1testapp/services/ArticleService.java
+++ tech1testapp/src/main/java/com/oxbey/tech1testapp/services/ArticleService.java
@@ -1,5 +1,6 @@
 package com.oxbey.tech1testapp.services;
 
+
 import com.oxbey.tech1testapp.entities.*;
 import com.oxbey.tech1testapp.exceptions.*;
 import com.oxbey.tech1testapp.repositories.ArticleRepository;
@@ -9,23 +10,23 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-@Service
-public class ArticleService {
-    @Autowired
-    UserRepository userRepository;
-    @Autowired
-    ArticleRepository articleRepository;
+ @Service
+ public class ArticleService {
+     @Autowired
+     UserRepository userRepository;
+     @Autowired
+     ArticleRepository articleRepository;
 
-    public ArticleEntity addArticle(ArticleEntity article, Long userId) {
-        UserEntity user = userRepository.findById(userId)
-                .orElseThrow(() -> new UserNotFoundException("User with id " + userId + " not found"));
-        article.setUser(user);
-        return articleRepository.save(article);
-    }
+     public ArticleEntity addArticle(ArticleEntity article, Long userId) {
+         UserEntity user = userRepository.findById(userId)
+                 .orElseThrow(() -> new UserNotFoundException("User with id " + userId + " not found"));
+         article.setUser(user);
+         return articleRepository.save(article);
+     }
 
-    public List<ArticleEntity> findAllByColour(String colour) {
-        List<ArticleEntity> articleEntities = articleRepository.findAllByColour(Colour.valueOf(Colour.class, colour))
-                .orElseThrow(() -> new IncorrectColourException("Article with colour " + colour + " not found or incorrect colour name"));
-        return articleEntities;
-    }
-}
+     public List<ArticleEntity> findAllByColour(String colour) {
+         List<ArticleEntity> articleEntities = articleRepository.findAllByColour(Colour.valueOf(Colour.class, colour))
+                 .orElseThrow(() -> new IncorrectColourException("Article with colour " + colour + " not found or incorrect colour name"));
+         return articleEntities;
+     }
+ }
